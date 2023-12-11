6fd80c857b6b0e6b6da1bcdc96e620f609960999
Mihretab <48807537+Almesty21@users.noreply.github.com>
2023-12-11 06:50:24 -0300

    Update toppings.js

--- models/toppings.js
+++ models/toppings.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const toppingsSchema = new mongoose.Schema(
 	{
-		name: { type: 'string', required: true },
+		name: { type: String, required: true },
 		stock: { type: Number, required: true },
 	},
 	{
